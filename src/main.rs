7eccfb84e0d03dd0385e490ef86ebb2495c5eb73
Skareeg Xigmatec <skareeg@gmail.com>
2020-03-22 22:54:00 -0400

    Switch to WGPU and add color lib.

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,4 @@ use rust_flow_lib::test;
 fn main() {
     println!("Hello, world!");
     test();
-
-    // use vulkano::device::{Device, DeviceExtensions, Features};
-
-    // let (device, mut queues) = {
-    //     Device::new(physical, &Features::none(), &DeviceExtensions::none(), [(queue_family, 0.5)].iter().cloned()).expect("failed to create device")
-    // };
-
-    // let queue = queues.next().unwrap();
 }
